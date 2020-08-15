a4e42350fc3a7df49651df5e7d943759fa893275
vagrant <vagrant@localhost.localdomain>
2020-08-15 09:54:17 +0000

    Update 5-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,17 +4,19 @@ class BooksController < ApplicationController
 		@book =Book.new
 	end
 	def create
-		@book =Book.new(book_params)
+		@book = Book.new(book_params)
 		@book.save
 		redirect_to books_path
 	end
 	def show
+		@book = Book.find(book_params[:id])
 	end
 	def edit
 	end
 	def index
+		@books = Book.all
 	end
-	
+
 	private
 	def book_params
 		params.require(:book).permit(:title, :body)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
   def show
-  	
+  	@user = User.find(params[:id])
+  	@books = @user.books.reverse_order
+  	@book =Book.new
   end
   def index
+  	@books = Book.all
   end
+
+  
 end
